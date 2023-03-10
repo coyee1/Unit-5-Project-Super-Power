7110a83b19d6788d477b66aba02e52052adb89b0
Colin <121971846+coyee1@users.noreply.github.com>
2023-03-09 17:02:46 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,22 @@
+let button = document.querySelector("button");
+button.onclick = function() {
+    let month = document.querySelector = (".input1").value;
+
+    if (month <= 6) {
+        document.querySelector = (".p").innerHTML = "yay";
+    } else if (month <= 12) {
+        document.querySelector = (".p").innerHTML = "yep";
+    } else {
+        document.querySelector = (".p").innerHTML = "try again";
+    }
+};
+button.onclick = function(){
+let Superpower = document.querySelector = ("input2").value;
+if (Superpower === "Thor") {
+    document.querySelector = (".result").innerHTML = "God of Thunder!";
+} if (Superpower === "Hulk") {
+    document.querySelector = (".result").innerHTML = "The Incredible Hulk!";
+}   else {
+    document.querySelector = (".result").innerHTML = "try again";
+    }
+};
